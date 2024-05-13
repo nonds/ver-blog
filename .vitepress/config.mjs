78b40db30d8e3298b89646a3c1c2e63b7a5d8aa9
搬砖小农 <244426288@qq.com>
2024-05-13 14:54:37 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,54 @@
+import { defineConfig } from 'vitepress'
+//import { set_sidebar } from '../utils/set_sidebar.mjs'
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: '/ver-blog/',
+  title: "VernonLiu技术站",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '知识技能', items:[
+        {text:'Vue',link:''}
+      ] },
+      { text: '扩展识海', link:''}
+    ],
+    sidebar:false,
+    // sidebar: [
+    //   {
+    //     text: 'Examples',
+    //     items: [
+    //       { text: 'Markdown Examples', link: '/markdown-examples' },
+    //       { text: 'Runtime API Examples', link: '/api-examples' }
+    //     ]
+    //   }
+    // ],
+    
+    search:{
+        provider:'local',
+        options:{
+          translations:{
+            button:{
+              buttonText:"搜文档",
+              buttonAriaLabel:"搜索文法"
+            },
+            modal:{
+              noResultsText:"无法找到相关结果",
+              resetButtonTitle:"清除查询条件",
+              footer:{
+                selectText:"选择",
+                navigateText:"切换"
+              }
+            }
+      
+          }
+        }
+    },
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/nonds' }
+    ],
+    footer:{
+      copyright:"Copyright@ 2024 Vernon Liu"
+    }
+  }
+})
---
+++ index.md
@@ -0,0 +1,32 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "VernonLiu的技术站"
+  text: "前端编程知识库"
+  tagline: Study hard and make progress every day
+  image:
+    src: /background.png
+    alt: 背景图
+  actions:
+    - theme: brand
+      text: 自我介绍
+      link: /markdown-examples
+      rel: alternate
+    - theme: alt
+      text: 杂记...
+      link: /api-examples
+
+features:
+  - icon: 💻   
+    title: 编码
+    details: 想写好前端，先练好内功。
+  - icon: 📚
+    title: 阅读
+    details: 前端技术如一门武功，文档则是了解这门武功最直观的方式，故不积跬步，无以至千里。
+  - icon: 🚀
+    title: 娱乐
+    details: 努力之后要学会放松自己，打打游戏，听听歌，旅旅游，陪陪家人。
+---
+
